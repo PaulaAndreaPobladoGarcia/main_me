ac8817500cf05b7c6bc3861196b439ba390c63d1
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-10-29 12:18:41 -0500

    tipo de dato python

---
+++ main.py
@@ -0,0 +1,13 @@
+FullName = "Paula Andrea Poblado Garcia" #Tipo de dato sting 
+age = 17 # Tipo de dato int 
+height = 1.74 #Tipo de dobule 
+camper = True #Tipo de dato boolean 
+
+
+# Conjuntos
+hobbies = ["cook", "draw", "play valorant"] #Tipo de dato array
+direction = (dict) ["country": "colombia", "state": "santander"] #Tipo de dato object 
+
+print( F""" Camper Name: "{FullName}" """ )
+
+ 
---
+++ mainp.py
@@ -0,0 +1,11 @@
+FullName = input ("Enter your name: " )
+age = int (input ("Enter your age: " ))
+gender = input ("Enter your gender M, F, X: ")
+heigth = float(input("Enter your height: "))
+
+print(f"""hola, usuario {FullName}. ¿cómo estás? te saluda tu mamá.
+Gracias por compartir tu edad {age}; no sabia que eras ese humano {gender} y su altura {heigth} mmm estas enano jijiji
+bueno adios mama ver**
+""")
+
+
